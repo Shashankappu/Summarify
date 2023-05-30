5b8a5f7cec9ecc6b5a713d7b68b070505f17dddb
Shashankappu <Shashankprince523@gmail.com>
2023-05-30 16:49:12 +0530

    splash activity added

---
+++ app/src/main/java/com/shashanksp/pocketpodcasts/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.shashanksp.pocketpodcasts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.shashanksp.pocketpodcasts.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySplashBinding binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);//splash time
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        //start thread
+        timerThread.start();
+    }
+}

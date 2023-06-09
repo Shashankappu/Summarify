a6c3e77070155f2fbbda5c60c6f62a19caee56c8
Shashankappu <Shashankprince523@gmail.com>
2023-06-09 15:14:04 +0530

    copy and paste functionality added

--- app/src/main/java/com/shashanksp/pocketpodcasts/SummarizeTextActivity.java
+++ app/src/main/java/com/shashanksp/pocketpodcasts/SummarizeTextActivity.java
@@ -2,19 +2,24 @@ package com.shashanksp.pocketpodcasts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.shashanksp.pocketpodcasts.databinding.ActivitySummarizeTextBinding;
 
 public class SummarizeTextActivity extends AppCompatActivity {
-
+    ClipboardManager clipboardManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivitySummarizeTextBinding binding = ActivitySummarizeTextBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
 
         binding.summarizeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +32,22 @@ public class SummarizeTextActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        binding.pasteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    ClipData.Item item = clipboardManager.getPrimaryClip().getItemAt(0);
+                    String output = item.getText().toString();
+
+                    if(!output.isEmpty()){
+                        output = binding.inputEdt.getText() +output;
+                        binding.inputEdt.setText(output);
+                        binding.inputEdt.moveCursorToVisibleOffset();
+                    }else{
+                        Toast.makeText(SummarizeTextActivity.this,"No Text to paste",Toast.LENGTH_LONG).show();
+                    }
             }
         });
     }
--- app/src/main/java/com/shashanksp/pocketpodcasts/SummarizedActivity.java
+++ app/src/main/java/com/shashanksp/pocketpodcasts/SummarizedActivity.java
@@ -2,23 +2,44 @@ package com.shashanksp.pocketpodcasts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.shashanksp.pocketpodcasts.databinding.ActivitySummarizedBinding;
 
-public class SummarizedActivity extends AppCompatActivity {
 
+public class SummarizedActivity extends AppCompatActivity {
+    ClipboardManager clipboardManager;
+    String input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivitySummarizedBinding binding = ActivitySummarizedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        // Initialize ClipboardManager
+        clipboardManager = (ClipboardManager) getSystemService(SummarizedActivity.CLIPBOARD_SERVICE);
+        binding.resText.setText("Summarized text appears here!");
         binding.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+        binding.copyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                input = binding.resText.getText().toString();
+                if(!input.isBlank()){
+                    ClipData data = ClipData.newPlainText("data",input);
+                    clipboardManager.setPrimaryClip(data);
+                    Toast.makeText(SummarizedActivity.this,"Text Copied",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(SummarizedActivity.this,"Text not Copied",Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
